b2def4c1d1c0c8c944b8ccef9ec362f0c8a5ba60
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2021-10-21 14:53:42 -0500

    introducing props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h2", { key: 1 }, props.name),
+    React.createElement("h3", { key: 2 }, props.breed),
+    React.createElement("h3", { key: 3 }, props.specie),
+  ]);
+};
 const App = () => {
-    return React.createElement(
-      "div",
-      {},
-      React.createElement("h1", {id: 'my-brand'}, "Adopt me")
-    );
-  };
-  ReactDOM.render(React.createElement(App), document.getElementById('root'));
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", { id: "my-brand" }, "Adopt me"),
+    ...[1,2,3].map(() => React.createElement("h1", {}, "Adopt me")),
+    [
+      React.createElement(Pet, {name: 'Shadow', specie: 'cat', breed: 'siamese'}),
+      React.createElement(Pet, {name: 'Santa', specie: 'dog', breed: 'boxed'})
+    ],
+  );
+};
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
