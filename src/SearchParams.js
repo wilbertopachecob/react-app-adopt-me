7c6d293973c58197b6259ce88e993db608df99b9
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2021-10-22 17:09:25 -0500

    added selects to searchp component

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,14 @@
 import { useState } from "react";
 
+const ANIMALS = ["dog", "cat", "bird", "lizard", "turtle"];
+
 const SearchParams = () => {
   const [location, setLocation] = useState("Broken Arrow, OK");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+
+  const breeds = [""];
+
   return (
     <div className="search-params">
       <form>
@@ -11,6 +18,38 @@ const SearchParams = () => {
             id="location"
             onChange={(e) => setLocation(e.target.value)}
           />
+        </label>
+        <label htmlFor="animals">
+          Animals
+          <select
+            value={animal}
+            id="animals"
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option value={animal} key={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breeds">
+          Breeds
+          <select
+            value={breed}
+            id="breeds"
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option value={breed} key={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
