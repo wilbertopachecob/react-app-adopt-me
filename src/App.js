674b335b6ac58b97503601d867d351397b942833
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2021-10-23 14:06:44 -0500

    enforcing strict mode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 // import React from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 // import Pet from "./Pet";
 import SearchParams from "./SearchParams";
@@ -26,12 +27,14 @@ import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
-    <div>
-      <h1 id="my-brand">Adopt Me</h1>
-      <SearchParams />
-      {/* <Pet name="Shadow" specie="cat" breed="siamese" />
-      <Pet name="Santa" specie="dog" breed="boxer" /> */}
-    </div>
+    <StrictMode>
+      <div>
+        <h1 id="my-brand">Adopt Me</h1>
+        <SearchParams />
+        {/* <Pet name="Shadow" specie="cat" breed="siamese" />
+          <Pet name="Santa" specie="dog" breed="boxer" /> */}
+      </div>
+    </StrictMode>
   );
 };
 
