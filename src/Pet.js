74156091e7848e3d457560ef938bdbcaba50cd8f
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2021-10-23 13:54:49 -0500

    refactored pets

--- src/Pet.js
+++ src/Pet.js
@@ -9,12 +9,22 @@
 // };
 
 const Pet = (props) => {
+  const { name, animal, breed, images, location, id } = props;
+
+  let src = "http://pets-images.dev-apis.com/pets/none.jpg";
+  if (images.length) {
+    src = images[0];
+  }
   return (
-    <div>
-      <h2>{props.name}</h2>
-      <h3>{props.breed}</h3>
-      <h3>{props.specie}</h3>
-    </div>
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={src} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} - ${breed} - ${location}`}</h2>
+      </div>
+    </a>
   );
 };
 
