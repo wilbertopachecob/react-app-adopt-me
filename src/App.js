543e1c4351a6fa370344c85ff7a82d67a395158f
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2021-10-24 11:28:40 -0500

    added router to app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 // import React from "react";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import Pet from "./Pet";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 // const App = () => {
 //   return React.createElement(
@@ -29,10 +31,17 @@ const App = () => {
   return (
     <StrictMode>
       <div>
-        <h1 id="my-brand">Adopt Me</h1>
-        <SearchParams />
-        {/* <Pet name="Shadow" specie="cat" breed="siamese" />
-          <Pet name="Santa" specie="dog" breed="boxer" /> */}
+        <Router>
+          <h1 id="my-brand">Adopt Me</h1>
+          <Switch>
+            <Route path="/details/:id">
+              <Details />
+            </Route>
+            <Route path="/">
+              <SearchParams />
+            </Route>
+          </Switch>
+        </Router>
       </div>
     </StrictMode>
   );
