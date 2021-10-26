5649d9a4855c251df2fbbeb8558ee5b245825922
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2021-10-25 22:11:11 -0500

    added loader

--- src/Results.js
+++ src/Results.js
@@ -1,25 +1,35 @@
 import Pet from "./Pet";
 import { STATES } from "./const/states";
 
-const Results = ({ pets, status }) => (
-  <div className="search">
-    {status === STATES.LOADING ? <div className="loader"></div> : ""}
-    {!pets ? (
-      <h1>No pets found</h1>
-    ) : (
-      pets.map((pet) => (
-        <Pet
-          animal={pet.animal}
-          key={pet.id}
-          location={`${pet.city}, ${pet.state}`}
-          images={pet.images}
-          breed={pet.breed}
-          id={pet.id}
-          name={pet.name}
-        />
-      ))
-    )}
-  </div>
-);
+const Results = ({ pets, status }) => {
+  console.log({status});
+  if (status === STATES.LOADING) {
+    return (
+      <div className="search">
+        <div className="loader"></div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="search">
+      {!pets ? (
+        <h1>No pets found</h1>
+      ) : (
+        pets.map((pet) => (
+          <Pet
+            animal={pet.animal}
+            key={pet.id}
+            location={`${pet.city}, ${pet.state}`}
+            images={pet.images}
+            breed={pet.breed}
+            id={pet.id}
+            name={pet.name}
+          />
+        ))
+      )}
+    </div>
+  );
+};
 
 export default Results;
