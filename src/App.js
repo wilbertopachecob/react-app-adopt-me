65e669b090ef4f54139d5c0e0c0f1f5a56bf9b61
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2021-10-28 14:17:23 -0500

    providing ThemeContext to the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 // import React from "react";
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 // import Pet from "./Pet";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
+import ThemeContext from "./ThemeContext";
 
 // const App = () => {
 //   return React.createElement(
@@ -28,25 +29,29 @@ import Details from "./Details";
 // };
 
 const App = () => {
+  const theme = useState("darkblue");
+
   return (
     <StrictMode>
-      <div>
-        <Router>
-          <header>
-            <Link to="/">
-              <h1 id="my-brand">Adopt Me</h1>
-            </Link>
-          </header>
-          <Switch>
-            <Route path="/details/:id">
-              <Details />
-            </Route>
-            <Route path="/">
-              <SearchParams />
-            </Route>
-          </Switch>
-        </Router>
-      </div>
+      <ThemeContext.Provider value={theme}>
+        <div>
+          <Router>
+            <header>
+              <Link to="/">
+                <h1 id="my-brand">Adopt Me</h1>
+              </Link>
+            </header>
+            <Switch>
+              <Route path="/details/:id">
+                <Details />
+              </Route>
+              <Route path="/">
+                <SearchParams />
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+      </ThemeContext.Provider>
     </StrictMode>
   );
 };
