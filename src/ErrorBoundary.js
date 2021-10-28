5ac3d9bfad144c55baf39f23868a0194149c74f9
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2021-10-28 06:35:29 -0500

    redirecting in ErrorBoundary

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 
 class ErrorBoundary extends Component {
-  state = { hasError: false };
+  state = { hasError: false, redirect: false };
 
   static getDerivedStateFromError() {
     return { hasError: true };
@@ -11,14 +11,17 @@ class ErrorBoundary extends Component {
   componentDidCatch(error, info) {
     this.setState({ hasError: true });
     console.error("ErrorBoundary caught error", error);
+    setTimeout(() => this.setState({ redirect: true }), 5000);
   }
 
   render() {
-    if (this.state.hasError) {
+    if (this.state.redirect) {
+      return <Redirect to="/"></Redirect>;
+    } else if (this.state.hasError) {
       return (
         <h2>
           Something went wrong. <Link to="/">Click here</Link> to go back to
-          homepage.
+          homepage or wait five seconds.
         </h2>
       );
     }
